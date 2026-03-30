2c03a9261bb19476fdfcfa48ee38046df304b554
mianather3 <mianather783@gmail.com>
2026-03-30 00:04:00 -0600

    Add iOS SwiftUI project scaffold

---
+++ PitWall/PitWall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PitWall
+//
+//  Created by Mian Ather Ali on 3/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PitWall/PitWall/PitWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitWallApp.swift
+//  PitWall
+//
+//  Created by Mian Ather Ali on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct PitWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
